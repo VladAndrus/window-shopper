7d363e3aac7e04390f59a22d2fa336aef4026827
Влад <vladyslav.andrus@gmail.com>
2019-07-14 16:54:21 +0200

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Влад on 7/14/19.
+//  Copyright © 2019 Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
